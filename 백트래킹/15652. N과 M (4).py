69f79de05ef886ac72b750528f39001549167c68
daeni-dang <de021@naver.com>
2022-01-04 22:49:23 +0900

    Add 15652. N과 M (4)

---
+++ 백트래킹/15652. N과 M (4).py
@@ -0,0 +1,15 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+p = []
+
+def f(start):
+    if len(p) == m:
+        sys.stdout.write(' '.join(map(str, p)) + '\n')
+        return
+    for i in range(start, n + 1):
+        p.append(i)
+        f(i)
+        p.pop()
+f(1)
