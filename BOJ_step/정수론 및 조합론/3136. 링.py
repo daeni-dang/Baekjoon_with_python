b5da077dd61c3cbbdbcde35a647e2a27c1ac6e34
daeni-dang <de021@naver.com>
2022-06-28 00:19:02 +0900

    Add 3136. 링

---
+++ BOJ_step/정수론 및 조합론/3136. 링.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    while b != 0:
+        n = a % b
+        a = b
+        b = n
+    return a
+
+n = int(input())
+ring = list(map(int, input().split()))
+for i in range(1, n):
+    common = gcd(ring[0], ring[i])
+    print('{0}/{1}'.format(ring[0] // common, ring[i] // common))
