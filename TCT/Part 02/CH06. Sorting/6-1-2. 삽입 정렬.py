0000e130d61a85c99c67268c409e4337e503a3bf
daeni-dang <de021@naver.com>
2022-08-07 16:26:51 +0900

    Add 삽입 정렬

---
+++ TCT/Part 02/CH06. Sorting/6-1-2. 삽입 정렬.py
@@ -0,0 +1,8 @@
+arr = list(map(int, input().split()))
+
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j - 1]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+        else: break
+print(arr)
