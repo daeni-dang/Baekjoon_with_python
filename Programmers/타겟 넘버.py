f359dddf96b133975d68f9283846a906e773807e
김다은_T4022 <de021@naver.com>
2023-02-25 00:19:32 +0900

    Add 타겟 넘버

--- Programmers/타겟 넘버.py
+++ Programmers/타겟 넘버.py
@@ -1,12 +1,12 @@
 def solution(numbers, target):
     answer = 0
-    def dfs(idx, result):
+    def dfs(idx, num):
+        nonlocal answer
         if idx == len(numbers):
-            if result == target:
-                nonlocal answer
+            if num == target:
                 answer += 1
-        else:
-            dfs(idx + 1, result + numbers[idx])
-            dfs(idx + 1, result - numbers[idx])
+            return
+        dfs(idx + 1, num + numbers[idx])
+        dfs(idx + 1, num - numbers[idx])
     dfs(0, 0)
     return answer
