46b07c16f991900af5d20f33db67983543080dc4
김다은_T4022 <de021@naver.com>
2023-07-31 21:48:29 +0900

    Add 11727. 2xn 타일링 2

---
+++ BOJ/Class 3/11727. 2xn 타일링 2.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n == 1:
+    print(1)
+else:
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 3
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007
+    print(dp[-1])
